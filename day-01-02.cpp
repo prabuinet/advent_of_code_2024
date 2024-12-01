e0a6eb1d757adf53dd629e4e0459e88eb4cce64e
prabu <prabuinet@gmail.com>
2024-12-01 13:19:00 +0530

    day 1

---
+++ day-01-02.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstdint>
+#include <unordered_map>
+
+
+using namespace std;
+
+int main()
+{
+  std::ifstream ifile("input2.txt");
+
+  std::vector<int> nums1;
+  std::vector<int> nums2;
+
+  while(!ifile.eof()) {
+    int num1, num2;
+    ifile >> num1 >> num2;
+    nums1.push_back(num1);
+    nums2.push_back(num2);
+  }
+
+  unordered_map<int, int> counts;
+  for (auto x : nums2) {
+    if (counts.find(x) == counts.end()) {
+      counts[x] = 1;
+    }
+    else 
+      counts[x]++;
+  }
+
+  long long total = 0;
+  for(auto x : nums1) {
+    if (counts.find(x) != counts.end())
+      total += counts[x] * x;
+  }
+
+  std::cout << total << endl;
+
+  return 0;
+}
